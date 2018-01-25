13432cf475ee022a8d5430e404b6253d7e76b1d1
Noga Kamil <kamil.noga@jcommerce.pl>
2018-01-25 23:24:23 +0100

    update

--- db-data/populate-games.js
+++ db-data/populate-games.js
@@ -6,7 +6,7 @@ games.remove({});
 
 games.insert([
   {
-    id: 'BOSS_PUZZLE',
+    id: 'BossPuzzle',
     name: 'Boss Puzzle',
     description: 'This is a sliding puzzle that consists of a frame of square tiles in random order with one tile missing. Also called Gem Puzzle or Mystic Square.'
   }
