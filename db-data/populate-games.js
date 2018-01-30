17deedebdf7eb9db964747732d12fcafaf5713f4
Noga Kamil <kamil.noga@jcommerce.pl>
2018-01-31 00:10:49 +0100

    new game

--- db-data/populate-games.js
+++ db-data/populate-games.js
@@ -10,5 +10,11 @@ games.insert([
     categoryId: 'sliding',
     name: 'Boss Puzzle',
     description: 'This is a sliding puzzle that consists of a frame of square tiles in random order with one tile missing. Also called Gem Puzzle or Mystic Square.'
+  },
+  {
+    id: 'EightQueens',
+    categoryId: 'chess',
+    name: 'Eight Queens',
+    description: 'The eight queens puzzle is the problem of placing eight chess queens on an 8×8 chessboard so that no two queens threaten each other. Thus, a solution requires that no two queens share the same row, column, or diagonal.'
   }
 ]);
