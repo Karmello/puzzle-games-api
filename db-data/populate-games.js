6e6a5f59b618ad97e1e12a9b6081eb1e4abd8317
Noga Kamil <kamil.noga@jcommerce.pl>
2018-02-03 17:46:18 +0100

    style -> mode

--- db-data/populate-games.js
+++ db-data/populate-games.js
@@ -12,7 +12,7 @@ games.insert([
     description: 'This is a sliding puzzle that consists of a frame of square tiles in random order with one tile missing. Also called Gem Puzzle or Mystic Square.',
     options: {
       dimension: ['3', '4', '5'],
-      style: ['IMG', 'NUM']
+      mode: ['IMG', 'NUM']
     }
   },
   {
