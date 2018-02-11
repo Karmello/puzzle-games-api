cb9b838e910d7053f11ca1a364095c96586557ac
Noga Kamil <kamil.noga@jcommerce.pl>
2018-02-11 16:48:24 +0100

    recreate.js

--- db-data/recreate.js
+++ db-data/recreate.js
@@ -1,1 +1,7 @@
 db = connect('ds211588.mlab.com:11588/heroku_rpzsr6b5', 'heroku_rpzsr6b5', 'msghahln2bgtfrfb5idd674vv6');
+
+const games = db.getCollection('games');
+const gamecategories = db.getCollection('gamecategories');
+
+games.remove({});
+gamecategories.remove({});
