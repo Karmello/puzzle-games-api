365c41866296760850289f90e53d1adb58bb1291
Noga Kamil <kamil.noga@jcommerce.pl>
2018-02-11 15:27:58 +0100

    recreate.js

--- db-data/recreate.js
+++ db-data/recreate.js
@@ -1,2 +1,1 @@
-const db = connect('ds211588.mlab.com:11588/heroku_rpzsr6b5', 'heroku_rpzsr6b5', 'msghahln2bgtfrfb5idd674vv6');
-console.log('Hello MongoDb !');
+db = connect('ds211588.mlab.com:11588/heroku_rpzsr6b5', 'heroku_rpzsr6b5', 'msghahln2bgtfrfb5idd674vv6');
