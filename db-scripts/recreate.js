ba5ef91efd43c1cc2913d65cf7d41339cd7d6b13
Noga Kamil <kamil.noga@jcommerce.pl>
2018-02-12 17:25:42 +0100

    updated recreate.js

--- db-scripts/recreate.js
+++ db-scripts/recreate.js
@@ -1,7 +1,8 @@
 load('db-data/gamecategories.js');
 load('db-data/games.js');
 
-db = connect('ds211588.mlab.com:11588/heroku_rpzsr6b5', 'heroku_rpzsr6b5', 'msghahln2bgtfrfb5idd674vv6');
+//db = connect('ds211588.mlab.com:11588/heroku_rpzsr6b5', 'heroku_rpzsr6b5', 'msghahln2bgtfrfb5idd674vv6');
+db = connect(process.env.MONGODB_URI);
 
 const gamecategories = db.getCollection('gamecategories');
 gamecategories.remove({});
