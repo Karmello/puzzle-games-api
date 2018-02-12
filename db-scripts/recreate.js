d6932fd1a6ab4f12d7338134b744acbb177126e1
Noga Kamil <kamil.noga@jcommerce.pl>
2018-02-12 17:12:56 +0100

    testing

--- db-scripts/recreate.js
+++ db-scripts/recreate.js
@@ -3,8 +3,6 @@ load('db-data/games.js');
 
 db = connect('ds211588.mlab.com:11588/heroku_rpzsr6b5', 'heroku_rpzsr6b5', 'msghahln2bgtfrfb5idd674vv6');
 
-
-
 const gamecategories = db.getCollection('gamecategories');
 gamecategories.remove({});
 gamecategories.insert(gamecategoriesData);
