99bcb3af6e2782a1d03a57445287372c4ac0b4d5
Kamil Noga <nogakamil@vp.pl>
2018-03-14 19:25:32 +0100

    issue#14: updated run.cmd and readme files

--- run.cmd
+++ run.cmd
@@ -1,9 +1,15 @@
 @echo off
 
-if "%1" == "recreate-db" (
+if "%1" == "db-dev-reset" (
+  cmd /c "mongo localhost:27017/puzzle-games < db/reset.js"
+)
 
-  if "%2" == "-dev" (
-    cmd /k "mongo localhost:27017/puzzle-games < db/recreate.js"
-    echo. & echo Done
-  )
+if "%1" == "db-test-reset" (
+  cmd /c "mongo ds155218.mlab.com:55218/test-puzzle-games -u %2 -p %3 < db/reset.js"
+)
+
+if "%1" == "db-dev-import-prod" (
+  cmd /c "mongo puzzle-games --eval db.dropDatabase()"
+  cmd /c "mongodump -h ds155218.mlab.com:55218 -d puzzle-games -u %2 -p %3 -o C:/mongodump"
+  cmd /c "mongorestore -h localhost:27017 -d puzzle-games C:/mongodump/puzzle-games"
 )
