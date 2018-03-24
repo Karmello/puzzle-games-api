64163cbeb20b177b8fd8c6a02d154b4784f25865
Kamil Noga <nogakamil@gmail.com>
2018-03-24 22:20:25 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -22,3 +22,8 @@ if "%1" == "db-dev-import-prod" (
   cmd /c "mongodump -h ds155218.mlab.com:55218 -d puzzle-games -u %2 -p %3 -o C:/mongodump"
   cmd /c "mongorestore -h localhost:27017 -d puzzle-games C:/mongodump/puzzle-games"
 )
+
+if "%1" == "db-prod-import-local" (
+  cmd /c "mongo ds155218.mlab.com:55218/puzzle-games -u %2 -p %3 --eval db.dropDatabase()"
+  cmd /c "mongorestore -h ds155218.mlab.com:55218 -d puzzle-games -u %2 -p %3 C:/mongodump/puzzle-games"
+)
