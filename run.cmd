3f77b4978c5bd8f96dfa047d13e4229d66a656c1
Kamil Noga <nogakamil@gmail.com>
2018-03-26 18:15:31 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -26,31 +26,35 @@ if %1 == db (
 
   if "%2" == "reset" (
     if !remote! == no (
+      echo * resetting %LOCAL_ADDRESS%/%LOCAL_DB%
       cmd /c mongo %LOCAL_ADDRESS%/%LOCAL_DB% < "db/reset.js"
     )
     if !remote! == yes (
+      echo * resetting %REMOTE_ADDRESS%/!REMOTE_DB!
       cmd /c mongo %REMOTE_ADDRESS%/!REMOTE_DB! -u %4 -p %5 < "db/reset.js"
     )
   )
 
   if %2 == dump (
     if !remote! == no (
-      echo * dumping from %LOCAL_ADDRESS%/%LOCAL_DB%
+      echo * dumping %LOCAL_ADDRESS%/%LOCAL_DB%
       cmd /c mongodump -h %LOCAL_ADDRESS% -d %LOCAL_DB% -o %FOLDER%
     )
     if !remote! == yes (
-      echo * dumping from %REMOTE_ADDRESS%/!REMOTE_DB!
+      echo * dumping %REMOTE_ADDRESS%/!REMOTE_DB!
       cmd /c mongodump -h %REMOTE_ADDRESS% -d !REMOTE_DB! -u %4 -p %5 -o %FOLDER%
     )
   )
 
   if %2 == import (
+    echo * importing from %REMOTE_ADDRESS%/!REMOTE_DB!
     cmd /c mongodump -h %REMOTE_ADDRESS% -d !REMOTE_DB! -u %4 -p %5 -o %FOLDER%
     cmd /c mongo %LOCAL_DB% --eval "db.dropDatabase()"
     cmd /c mongorestore -h %LOCAL_ADDRESS% -d %LOCAL_DB% %FOLDER%/%LOCAL_DB%
   )
 
   if %2 == export (
+    echo * exporting to %REMOTE_ADDRESS%/!REMOTE_DB!
     cmd /c mongodump -h %LOCAL_ADDRESS% -d %LOCAL_DB% -o %FOLDER%
     cmd /c mongo %REMOTE_ADDRESS%/!REMOTE_DB! -u %4 -p %5 --eval "db.dropDatabase()"
     cmd /c mongorestore -h %REMOTE_ADDRESS% -d !REMOTE_DB! -u %4 -p %5 %FOLDER%/%LOCAL_DB%
