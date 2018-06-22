a1ccee43eaaff0347b0c41ed458cb7442f66393c
Kamil Noga <nogakamil@gmail.com>
2018-06-22 22:12:04 +0200

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,13 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 
+if %1 == pro (
+  echo * starting puzzle-games-api
+  cmd /c "mongod" -new_console:t:"mongod"
+  cmd /c "nodemon server.js" -new_console:t:"api"
+  goto:end
+)
+
 if %1 == db (
 
   SET LOCAL_ADDRESS=localhost:27017
@@ -26,37 +33,39 @@ if %1 == db (
 
   if "%2" == "reset" (
     if !remote! == no (
-      echo * resetting %LOCAL_ADDRESS%/%LOCAL_DB%
-      cmd /c mongo %LOCAL_ADDRESS%/%LOCAL_DB% < "db/reset.js"
+      echo * resetting !LOCAL_ADDRESS!/!LOCAL_DB!
+      cmd /c mongo !LOCAL_ADDRESS!/!LOCAL_DB! < "db/reset.js"
     )
     if !remote! == yes (
-      echo * resetting %REMOTE_ADDRESS%/!REMOTE_DB!
-      cmd /c mongo %REMOTE_ADDRESS%/!REMOTE_DB! -u %4 -p %5 < "db/reset.js"
+      echo * resetting !REMOTE_ADDRESS!/!REMOTE_DB!
+      cmd /c mongo !REMOTE_ADDRESS!/!REMOTE_DB! -u %4 -p %5 < "db/reset.js"
     )
   )
 
   if %2 == dump (
     if !remote! == no (
-      echo * dumping %LOCAL_ADDRESS%/%LOCAL_DB%
-      cmd /c mongodump -h %LOCAL_ADDRESS% -d %LOCAL_DB% -o %FOLDER%
+      echo * dumping !LOCAL_ADDRESS!/!LOCAL_DB!
+      cmd /c mongodump -h !LOCAL_ADDRESS! -d !LOCAL_DB! -o !FOLDER!
     )
     if !remote! == yes (
-      echo * dumping %REMOTE_ADDRESS%/!REMOTE_DB!
-      cmd /c mongodump -h %REMOTE_ADDRESS% -d !REMOTE_DB! -u %4 -p %5 -o %FOLDER%
+      echo * dumping !REMOTE_ADDRESS!/!REMOTE_DB!
+      cmd /c mongodump -h !REMOTE_ADDRESS! -d !REMOTE_DB! -u %4 -p %5 -o !FOLDER!
     )
   )
 
   if %2 == import (
-    echo * importing from %REMOTE_ADDRESS%/!REMOTE_DB!
-    cmd /c mongodump -h %REMOTE_ADDRESS% -d !REMOTE_DB! -u %4 -p %5 -o %FOLDER%
-    cmd /c mongo %LOCAL_DB% --eval "db.dropDatabase()"
-    cmd /c mongorestore -h %LOCAL_ADDRESS% -d %LOCAL_DB% %FOLDER%/%LOCAL_DB%
+    echo * importing from !REMOTE_ADDRESS!/!REMOTE_DB!
+    cmd /c mongodump -h !REMOTE_ADDRESS! -d !REMOTE_DB! -u %4 -p %5 -o !FOLDER!
+    cmd /c mongo !LOCAL_DB! --eval "db.dropDatabase()"
+    cmd /c mongorestore -h !LOCAL_ADDRESS! -d !LOCAL_DB! !FOLDER!/!LOCAL_DB!
   )
 
   if %2 == export (
-    echo * exporting to %REMOTE_ADDRESS%/!REMOTE_DB!
-    cmd /c mongodump -h %LOCAL_ADDRESS% -d %LOCAL_DB% -o %FOLDER%
-    cmd /c mongo %REMOTE_ADDRESS%/!REMOTE_DB! -u %4 -p %5 --eval "db.dropDatabase()"
-    cmd /c mongorestore -h %REMOTE_ADDRESS% -d !REMOTE_DB! -u %4 -p %5 %FOLDER%/%LOCAL_DB%
+    echo * exporting to !REMOTE_ADDRESS!/!REMOTE_DB!
+    cmd /c mongodump -h !LOCAL_ADDRESS! -d !LOCAL_DB! -o !FOLDER!
+    cmd /c mongo !REMOTE_ADDRESS!/!REMOTE_DB! -u %4 -p %5 --eval "db.dropDatabase()"
+    cmd /c mongorestore -h !REMOTE_ADDRESS! -d !REMOTE_DB! -u %4 -p %5 !FOLDER!/!LOCAL_DB!
   )
 )
+
+:end
