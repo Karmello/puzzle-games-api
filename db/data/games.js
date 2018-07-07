43ab7a5a5b73479807b4bed836edd7860685c1cb
Karmello <nogakamil@vp.pl>
2018-07-07 19:54:15 +0200

    #37 - updated game hardcoded data

--- db/data/games.js
+++ db/data/games.js
@@ -29,9 +29,9 @@ const gamesData = [
     id: 'sudoku',
     categoryId: 'logic',
     name: 'Sudoku',
-    description: '',
+    description: 'Sudoku (originally called Number Place) is a logic-based, combinatorial number-placement puzzle.',
     options: {},
-    info: ''
+    info: 'The objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 subgrids that compose the grid (also called "boxes", "blocks", or "regions") contains all of the digits from 1 to 9.'
   }
 ];
 
