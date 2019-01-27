cb1129e4faac1f74ce26d06d10ce9c19cfb39508
Karmello <nogakamil@vp.pl>
2019-01-27 22:28:22 +0100

    #46 - updated hardcoded data

--- db/data/games.js
+++ db/data/games.js
@@ -35,6 +35,14 @@ const gamesData = [
     description: 'Sudoku (originally called Number Place) is a logic-based, combinatorial number-placement puzzle.',
     options: {},
     info: 'The objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 subgrids that compose the grid (also called "boxes", "blocks", or "regions") contains all of the digits from 1 to 9.'
+  },
+  {
+    id: 'tetris',
+    categoryId: 'falling-block',
+    name: 'Tetris',
+    description: '...',
+    options: {},
+    info: '...'
   }
 ];
 
